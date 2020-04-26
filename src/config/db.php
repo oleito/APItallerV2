568b952216010fb046e5dab3fcac5053cc4eb4fd
Leandro Nicolas <oleito@gmail.com>
2020-04-26 19:30:48 -0300

    asd

--- src/config/db.php
+++ src/config/db.php
@@ -1,6 +1,6 @@
 <?php
 /** BASE DE DATOS MYSQL **/
 define('MySQL_DB_HOST', 'cdgweb.com.ar');
-define('MySQL_DB_USER', 'cdgwebco_ripa_user');
-define('MySQL_DB_PASS', '+-M^e*E1L[o#');
-define('MySQL_DB_SCHEMA', 'cdgwebco_ripa_usuarios');
+define('MySQL_DB_USER', 'cdgwebco_api_tallerv2');
+define('MySQL_DB_PASS', 'M3SB!Wj9_YQ3');
+define('MySQL_DB_SCHEMA', 'cdgwebco_tallerv2');
