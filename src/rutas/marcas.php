d0825c542103875e3cfe4cf37dbe10e33b5772d1
Leandro Nicolas <oleito@gmail.com>
2020-04-29 21:23:26 -0300

    Crea grupo de ruta

--- src/rutas/marcas.php
+++ src/rutas/marcas.php
@@ -1,48 +1,47 @@
 <?php
-use \Psr\Http\Message\ResponseInterface as Response;
-use \Psr\Http\Message\ServerRequestInterface as Request;
 
 /**
  * RUTA
  * LOGIN
  */
 
-$app->get('/marcas', function (Request $request, Response $response, array $args) {
-    if ($request->getAttribute('isLoggedIn') === 'true') {
-        $rp['token'] = $request->getAttribute('newToken');
+$app->group('/marcas', function () use ($app) {
+    $app->get('', function ($request, $response) {
+        if ($request->getAttribute('isLoggedIn') === 'true') {
+            $rp['token'] = $request->getAttribute('newToken');
 
-        $conn = new pdoMysql($this->logger);
-        try {
-            $sql = "SELECT * FROM vhMarca";
-            $res = $conn->querySQL($sql, []);
+            $conn = new pdoMysql($this->logger);
+            try {
+                $sql = "SELECT * FROM vhMarca";
+                $res = $conn->querySQL($sql, []);
 
-        } catch (Exception $e) {
-            $this->logger->warning('/marcas () - ', [$e->getMessage()]);
-            return 500;
+            } catch (Exception $e) {
+                $this->logger->warning('/marcas () - ', [$e->getMessage()]);
+                return 500;
+            }
+
+            if ($res === 400) {
+                return $response->withHeader('Content-type', 'application/json')
+                    ->withStatus(400)
+                    ->withJson(null);
+            } else if ($res === 404) {
+                return $response->withHeader('Content-type', 'application/json')
+                    ->withStatus(404)
+                    ->withJson(null);
+            } else if ($res === 500) {
+                return $response->withHeader('Content-type', 'application/json')
+                    ->withStatus(500)
+                    ->withJson(null);
+            }
+
+            $rp['data'] = $res;
+            return $response->withHeader('Content-type', 'application/json')
+                ->withStatus(200)
+                ->withJson($rp);
         }
-
-        if ($res === 400) {
-            return $response->withHeader('Content-type', 'application/json')
-                ->withStatus(400)
-                ->withJson(null);
-        } else if ($res === 404) {
-            return $response->withHeader('Content-type', 'application/json')
-                ->withStatus(404)
-                ->withJson(null);
-        } else if ($res === 500) {
-            return $response->withHeader('Content-type', 'application/json')
-                ->withStatus(500)
-                ->withJson(null);
-        }
-
-        $rp['data'] = $res;
         return $response->withHeader('Content-type', 'application/json')
-            ->withStatus(200)
-            ->withJson($rp);
-
-    }
-    return $response->withHeader('Content-type', 'application/json')
-        ->withStatus(401)
-        ->withJson(null);
+            ->withStatus(401)
+            ->withJson(null);
+    });
 
 })->add($guardMiddleware);
